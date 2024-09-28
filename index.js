75727a70cc91fe238c5f897f5753608c811720e0
Cristian <chris05.es@gmail.com>
2024-09-28 17:12:19 +0200

    before await

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Running");
 
 const url = "https://api.open-meteo.com/v1/forecast?latitude=42.4667&longitude=-2.45&hourly=temperature_2m&timezone=auto&forecast_days=1";
 const openCageKey = "9bef2c108e834b51a76d21be45b334f0";
+const openCageApiUrl = "https://api.opencagedata.com/geocode/v1/json";
 
 function getTime() {            // Obtains user's time to display
     const now = new Date();
@@ -19,14 +20,32 @@ const {stringTime} = getTime();
 
 document.getElementById("horalocal").innerHTML = stringTime;        // Displays time
 
+let userLatitude;
+
+let userLongitude;
+
 function positionSuccess(pos) {
     console.log(pos);
-    const {latitude} 
+    userLatitude = pos.coords.latitude;
+    userLongitude = pos.coords.longitude;
 }
 
 navigator.geolocation.getCurrentPosition(positionSuccess);         // Obtains user's coordinates
 
+console.log(userLatitude);
 
+let query = userLatitude + "," + userLongitude;
+
+console.log(query);
+
+const requestUrl = openCageApiUrl + "?" + "key=" + openCageKey + "&q=" + query + "&pretty=1" + "&no_annotations=1";
+
+console.log(requestUrl);
+
+/*fetch(requestUrl)
+    .then(response => responsecoords.json)
+    .then(data => console.log(data))
+    .catch(error => console.error("Error: ", error)); */
 
 /*fetch(url)
     .then(response => response.json())
